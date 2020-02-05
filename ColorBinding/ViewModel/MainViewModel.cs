734ae12a97494b4ac88fe1d583bf45551630d4a3
Pepa <josef.simunek@pslib.cz>
2020-02-05 09:38:59 +0100

    ahoj

---
+++ ColorBinding/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace ColorBinding.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private double _red;
+        private double _green;
+        private double _blue;
+        private SolidColorBrush _selectedcolor;
+
+        public MainViewModel()
+        {
+            Red = 100;
+            Green = 50;
+            Blue = 10;
+        }
+
+        public double Red { get { return _red; } set { _red = value; NotifyPropertyChanged(); } }
+        public double Green { get { return _green; } set { _blue = value; NotifyPropertyChanged(); } }  
+        public double Blue  { get { return _blue;  } set {_blue = value; NotifyPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+
+    }
+}
